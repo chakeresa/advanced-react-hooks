bc068c89e3f6b3af9e502ada9cf82f297c358314
Alexandra Chakeres <47309861+chakeresa@users.noreply.github.com>
2021-04-09 15:41:31 -0600

    Change countReducer

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -8,12 +8,12 @@ function Counter({initialCount = 0, step = 1}) {
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  function countReducer(previousCount, newCount) {
-    return newCount
+  function countReducer(previousCount, step) {
+    return previousCount + step
   }
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
 
-  const increment = () => setCount(count + step)
+  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
