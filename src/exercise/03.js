8f5e461289a1753c551ec6ba370af1065a688581
Alexandra Chakeres <47309861+chakeresa@users.noreply.github.com>
2021-04-28 16:41:00 -0600

    Implement create/useContext & make provider
    Ex03 soln

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,25 +1,30 @@
+// Questions for the group:
+//  - Why is useContext better suited to libraries vs individual apps (according to KCD)?
+//       I kinda hate the composition thing that the Twitter guy used in the linked video.
+//       It made the components look super bloated / removed some nice abstraction.
+//       What if it was an element in a generated list that needed the current user?
+
 // useContext: simple Counter
 // http://localhost:3000/isolated/exercise/03.js
 
 import * as React from 'react'
 
-// 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext()
 
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+function CountProvider(props) {
+  const countAndSetCount = React.useState(0)
+  return <CountContext.Provider value={countAndSetCount} {...props} />
+}
 
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = React.useContext(CountContext)
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  // eslint-disable-next-line
+  const [count, setCount] = React.useContext(CountContext)
+
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -27,12 +32,10 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
